b828c697c3ebc3af45c62f53f1cfbfe21b9c0ad4
IdoAtlas <ido.atlas@gmail.com>
2025-03-12 19:25:51 +0200

    added some graphes, added best PESQ checkpoint for fusion model

--- pretrained_fusion.sh
+++ pretrained_fusion.sh
@@ -1,7 +1,7 @@
-CUDA_VISIBLE_DEVICES='0' python inference_ofer.py \
+CUDA_VISIBLE_DEVICES='0' python inference_fusion.py \
    --input_folder  /mnt/d/Projects/Deep_Learning/final_project/databases/voicebank-demand/noisy_testset_wav \
    --output_folder results/fusion/testset \
-   --checkpoint_file exp_fusion/SEMamba_fusion_v1/g_00530000.pth  \
+   --checkpoint_file ckpts/Semamba_fusion_00138000_PESQ_3_291.pth  \
    --config recipes/SEMamba_fusion/SEMamba_fusion.yaml \
    --post_processing_PCS False \
    
